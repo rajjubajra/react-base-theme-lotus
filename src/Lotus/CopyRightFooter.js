658ace33f928c422b342e9cbbf8a1f6533143bf2
rajju <vajrachara@yahoo.com>
2020-03-08 10:05:40 +0000

    readme file udate

---
+++ src/Lotus/CopyRightFooter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const CopyRightFooter = () => {
+  const copyrightStyle = {
+    textAlign: "center",
+    width: "100%",
+    fontFamily: "sans-serif",
+    fontSize: "11px"
+  }
+
+  const weblink = {
+    textDecoration: "none",
+    color: "#000"
+  }
+  return (
+    <>
+      <div style={copyrightStyle}>
+        <p>
+          &copy; {new Date().getFullYear()}
+          All Right Reserved | Desinged and developed by
+          <a href="http://yellow-website.com" style={weblink} >Yellow-Website.com</a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default CopyRightFooter
